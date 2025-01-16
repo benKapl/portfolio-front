0f7ea30feaa56ca88bf0452040e693537e2be1da
benKapl <benji1.kaplan@gmail.com>
2025-01-16 17:22:03 +0100

    creating app structure

---
+++ src/app/(content)/about/bio/page.js
@@ -0,0 +1,7 @@
+export default function Bio() {
+    return (
+        <div className="bg-slate-500 flex flex-1 justify-center items-center">
+            <h1>BIOGRAPHIE</h1>
+        </div>
+        )
+} 
---
+++ src/app/(content)/about/extra/page.js
@@ -0,0 +1,7 @@
+export default function Extra() {
+    return (
+        <div className="bg-slate-500 flex flex-1 justify-center items-center">
+            <h1>EXTRA</h1>
+        </div>
+        )
+} 
