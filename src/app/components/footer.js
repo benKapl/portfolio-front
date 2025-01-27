d04701ad62d17cdb6cb0bc2cf52db2cdd16dbb18
benKapl <benji1.kaplan@gmail.com>
2025-01-27 17:59:41 +0100

    deployment failed with server component, tried fetching on client compenent

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,16 +1,35 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
 
-export const Socials = async () => {
-    const response = await fetch(`${backendUrl}/socials`)
-    const socials = await response.json()
+export const Socials = () => {
+
+    const [socials, setSocials] = useState([]);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        // Fetch Socials
+        const fetchSocials = async () => {
+            try {
+                const response = await fetch(`${backendUrl}/socials`)
+                if (!response.ok) throw new Error("Failed to fetch socials");
+                const data = await response.json()
+                console.log(data)
+                setSocials(data.socials)
+            } catch (err) {
+                setError("Failed to fetch socials")
+            }
+        }
+        fetchSocials()
+    }, [])
 
     return (
         <div className="m-4 flex gap-12 text-white">
             <p>Connect with me: </p>
-        {socials.data.map((data, index) => (
+        {socials.map((data, index) => (
             <Link key={index} href={data.url} passHref target="_blank">
                 <Image
                     src={`/images/logos/${data.logoUrl}`}
