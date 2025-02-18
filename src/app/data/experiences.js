74f6e8264951748ecf64d997b947a59c90c8f03c
benKapl <benji1.kaplan@gmail.com>
2025-02-18 17:56:15 +0100

    updated experiences, added sogedcev and lacapsule (not really an experience but, you know...)

--- src/app/data/experiences.js
+++ src/app/data/experiences.js
@@ -1,5 +1,15 @@
 export const companies = [
   {
+    id: "capsule",
+    name: "La Capsule",
+    logo: "logo_capsule.png",
+    website: "https://www.lacapsule.academy/",
+    description: {
+      FR: "La Capsule est un Coding Bootcamp qui propose des formations de haut niveau pour apprendre à coder en web et mobile sur des formations intensives de 10 semaines.",
+      EN: ""
+    },
+  },
+  {
     id: "energisme",
     name: "Energisme",
     logo: "logo_energisme.png",
@@ -18,10 +28,66 @@ export const companies = [
       FR: "Solutions web et mobile de création vidéo autonome pour les salariés de grands groupes.",
       EN: ""
     },
+  },
+  {
+    id: "sogedev",
+    name: "Sogedev",
+    logo: "logo_sogedev.png",
+    website: "https://www.sogedev.com",
+    description: {
+      FR: "Cabinet de conseil en financement de l'innovation pour les PMEs et startups.",
+      EN: ""
+    },
   }
 ]
 
 export const jobs = [
+  // LA CAPSULE
+  {
+    category: "development",
+    companyId: "capsule",
+    title: {
+      FR: "Etudiant en développement web fullstack",
+      EN: "Student fullstack developer",
+    },
+    // description: {
+    //   FR: "Responsable du développement de solutions techniques (construction d'ETLs) autour de la brique 'Data Collect'",
+    //   EN: ""
+    // },
+    startDate: new Date(2024, 9, 14),
+    endDate: new Date(2024, 11, 21),
+    tasks: {
+      FR: [
+        "Conception de sites web en Javascript Vanilla",
+        "Apprentissage des frameworks modernes de développement web frontend et backend",
+        "Développement d'application mobiles",
+        "Mise en pratique de l'architecture MVC",
+        "Mise en pratique du Test Driven Development",
+        "Obtention du titre RNCP 6 équivalence Bac +4",
+      ],
+      EN: [
+      ]
+    },
+    stack: {
+      languages: [
+        "js",
+        "ts",
+        "htmlcss",
+        "react",
+        "nextjs",
+        "expressjs",
+        "mongodb",
+        "redux",
+        "tailwind",
+    ],
+      tools: [
+        "Notion",
+        "Workbench",
+        "ElasticSearch",
+        "Apache Nifi"
+      ]
+    }
+  },
   // ENERGISME
   {
     category: "product",
@@ -116,6 +182,36 @@ export const jobs = [
         "Accompagnement client BtoB",
         "Animation de formations",
         "Tournages et montages vidéos",
+      ],
+      EN: [
+      ]
+    },
+    stack: {
+      languages: [
+    ],
+      tools: [
+        "Notion",
+        "n8n",
+      ]
+    }
+  },
+  {
+    category: "account",
+    companyId: "sogedev",
+    title: {
+      FR: "Chargé d'affaires",
+      EN: "Business developer",
+    },
+    // description: {
+    //   FR: "J'ai démarré à l'origine chez Pitchy en tant que CSM. <br>Mon rôle était d'accompagner les clients et utilisateurs dans leur stratégie de communication par la vidéo",
+    //   EN: ""
+    // },
+    startDate: new Date(2015, 10, 2),
+    endDate: new Date(2018, 10, 30),
+    tasks: {
+      FR: [
+        "Développement commercial",
+        "Audit d'éligibilité aux aidesnu",
       ],
       EN: [
       ]
