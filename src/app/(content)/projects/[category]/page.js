1e0fae691de8318345ddb8d040df73fcbfc83a24
benKapl <benji1.kaplan@gmail.com>
2025-02-19 18:42:37 +0100

    project are now visible, next step : full content and proper layout

--- src/app/(content)/projects/[category]/page.js
+++ src/app/(content)/projects/[category]/page.js
@@ -8,31 +8,34 @@ export default async function ProjectPage({params}) {
 
   const { category } = await params
 
-    // IMPORTANT : TROUVER UN MOYEN POUR FAIRE MATCHER LES PROJETS AVEC
-    // LA CATÉGORIE DEFINIE DANS L'URL
+  let filteredProjects
+  !isNaN(Number(category)) ?  // Checks that category IS a number
+    filteredProjects = projects.filter(data => data.year == category) : // Filter project based on year
+    filteredProjects = projects.filter(data => data.isFavorite === true);   // If url is not year, retrieve favorites
 
-    // Styles
-    const container = "bg-inherit flex-1 flex-col h-auto w-full justify-start items-start mx-4 w-[90%]"
-    const titleStyle = "text-slate-300 font-bold"
-    const projectsContainer = "flex bg-inherit flex-col h-auto w-auto mx-4 gap-2"
 
-    return (
-        <section className={container}>
-            <div className="sticky top-0 pt-6 pb-4 pl-6 bg-inherit h-18 w-full z-30"> {/*Fixes the title at the top*/}
-                <h1 className={clsx(titleStyle, "text-3xl")}>{category}</h1>
-            </div>
-            <div className={projectsContainer}>
-                <Project />
-                {/* {projects.map((data) => {
-                  return (<Project
-                    key={data.id}
-                    id={data.id}
-                    name={data.name}
-                    logo={`/images/logos/${data.logo}`}
-                    website={data.website}
-                    description={data.description}
-                    />)})} */}
-            </div>
-        </section>
-        )
+  // Styles
+  const container = "bg-inherit flex-1 flex-col h-auto w-full justify-start items-start mx-4 w-[90%]"
+  const titleStyle = "text-slate-300 font-bold"
+  const projectsContainer = "flex bg-inherit flex-col h-auto w-auto mx-4 gap-2"
+
+  return (
+      <section className={container}>
+          <div className="sticky top-0 pt-6 pb-4 pl-6 bg-inherit h-18 w-full z-30"> {/*Fixes the title at the top*/}
+              <h1 className={clsx(titleStyle, "text-3xl")}>{category}</h1>
+          </div>
+          <div className={projectsContainer}>
+            {filteredProjects.map(data => {
+              return (<Project
+                  key={data.id}
+                  name={data.name}
+                  description={data.description}
+                  demoUrl={`/gifs/${data.image}`}
+                  website={data.website}
+                  repo={data.repo}
+                  stack={data.stack}
+                  />)})}
+          </div>
+      </section>
+    )
 }
--- src/app/components/project.js
+++ src/app/components/project.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import clsx from "clsx"
 
-export const Project = ({ name, description, demo, website, repo, stack }) => {
+export const Project = ({ name, description, demoUrl, website, repo, stack }) => {
 
     const container = "flex flex-col w-full h-auto px-6 py-3 bg-inherit "
     const titleStyle = "text-slate-300 font-bold"
@@ -13,10 +13,10 @@ export const Project = ({ name, description, demo, website, repo, stack }) => {
 
     return (
         <div className={container}>
-            <h2 className={clsx(titleStyle)}>{name}</h2>
+            <h2 className={clsx(titleStyle)}>{name.FR}</h2>
             <div className={mainContent}>
-                <Image src={`/gifs/${demo}`} alt={`Demo ${name}`} width={300} height={100}/>
-                <div className={textContent}>{description}</div>
+                <Image src={demoUrl} alt={`Demo ${name}`} width={300} height={100}/>
+                <div className={textContent}>{description.FR}</div>
             </div>
             <div className={stacksContainer}></div>
             <div className={linksContainer}></div>
