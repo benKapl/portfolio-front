6afccfaa986e4493be0fa2303493eafc06ba3673
benKapl <benji1.kaplan@gmail.com>
2025-02-20 09:30:28 +0100

    added project's description formatting based on line return

--- src/app/components/project.js
+++ src/app/components/project.js
@@ -4,6 +4,8 @@ import clsx from "clsx"
 
 export const Project = ({ name, description, demoUrl, website, repo, stack }) => {
 
+    const lines = description.FR.split("\n");
+
     const container = "flex flex-col w-full h-auto px-6 py-3 bg-inherit "
     const titleStyle = "text-slate-300 font-bold"
     const mainContent = "flex w-full h-auto justify-start items-center"
@@ -16,7 +18,14 @@ export const Project = ({ name, description, demoUrl, website, repo, stack }) =>
             <h2 className={clsx(titleStyle)}>{name.FR}</h2>
             <div className={mainContent}>
                 <Image src={demoUrl} alt={`Demo ${name}`} width={300} height={100}/>
-                <div className={textContent}>{description.FR}</div>
+                <div className={textContent}>
+                    {lines.map((data, i) => {
+                        let lastLine = lines[lines.length - 1]
+                        let htmlLine = <p key={i}>{data}</p>
+                        if (data != lastLine) { return <>{htmlLine}<br/></>} // If not lastline, add a return
+                        return htmlLine
+                     })}
+                </div>
             </div>
             <div className={stacksContainer}></div>
             <div className={linksContainer}></div>
